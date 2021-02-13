2e2c42aada6766cac1ebab967c35fd019351e3fe
sonkyokukou <xu@sun-its.com>
2021-02-13 15:06:09 +0900

    fix bug

--- start.sh
+++ start.sh
@@ -25,7 +25,7 @@ fi
 # Check if default.pem has been created
 if [ ! -e ${DEFAULT_PEM} ]; then
   openssl genrsa -des3 -passout pass:${PASSWORD} -out ${KEY} 2048 &> /dev/null
-  openssl req -new -key ${KEY} -passin pass:${PASSWORD} -out ${CSR} -subj ${SUBJECT} &> /dev/null
+  openssl req -new -key ${KEY} -passin pass:${PASSWORD} -out ${CSR} -subj "${SUBJECT}" &> /dev/null
   cp ${KEY} ${KEY}.org &> /dev/null
   openssl rsa -in ${KEY}.org -passin pass:${PASSWORD} -out ${KEY} &> /dev/null
   openssl x509 -req -days 3650 -in ${CSR} -signkey ${KEY} -out ${CERT} &> /dev/null
